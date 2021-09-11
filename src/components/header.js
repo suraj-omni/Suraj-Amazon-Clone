03d74c8d952635644e2b55d916551130af65bef2
suraj-omni <suraj@omnitechnologies.lk>
2021-09-11 23:32:15 +0530

    testing header issue
    testing 2

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,10 @@
 import React from "react";
-/* import Image from "next/image";
+import Image from "next/image";
 import {
   MenuIcon,
   SearchIcon,
   ShoppingCartIcon,
-} from "@heroicons/react/outline"; */
+} from "@heroicons/react/outline";
 import { signIn, signOut, useSession } from "next-auth/client";
 import { selectItems } from "../slices/cartSlice";
 import { useRouter } from "next/router";
@@ -16,7 +16,79 @@ function Header() {
   const router = useRouter();
   const items = useSelector(selectItems);
   return (
-   <div>Header</div>
+    <header>
+      {/* Top Navigation */}
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+          <Image
+            onClick={() => router.push("/")}
+            src={url}
+            width={150}
+            height={40}
+            objectFit="contain"
+            className="cursor-pointer"
+          />
+        </div>
+
+        {/* Product Search */}
+        <div
+          className="hidden bg-yellow-400 hover:bg-yellow-500
+          sm:flex
+          items-center
+          h-10
+          rounded-md
+          flex-grow
+          cursor-pointer focus:outline-none"
+        >
+          <input
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md px-4"
+            type="text"
+          ></input>
+          <SearchIcon className="h-12 p-4" />
+        </div>
+
+        {/* Right Section */}
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div onClick={!session ? signIn : signOut} className="link">
+            <p>{session ? `Hello, ${session.user.name}` : `Sign In`}</p>
+            <p className="font-extrabold md:text-sm">Accounts & Lists</p>
+          </div>
+          <div className="link">
+            <p>Returns</p>
+            <p className="font-extrabold md:text-sm"> & Orders</p>
+          </div>
+          <div
+            onClick={() => router.push("/checkout")}
+            className="relative link flex items-center"
+          >
+            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
+              {items.length}
+            </span>
+            <ShoppingCartIcon className="h-10" />
+            <p className="hidden md:inline font-extrabold md:text-sm mt-2">
+              Basket
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Bottom Navigation */}
+      <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
+        <p className="link flex items-center">
+          <MenuIcon className="h-6 mr-1" />
+          All
+        </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Today's Deals</p>
+        <p className="link hidden lg:inline-flex">Electronics</p>
+        <p className="link hidden lg:inline-flex">Food & Grocery</p>
+        <p className="link hidden lg:inline-flex">Gift Cards</p>
+        <p className="link hidden lg:inline-flex">Registry</p>
+        <p className="link hidden lg:inline-flex">Customer Service</p>
+        <p className="link hidden lg:inline-flex">Sell</p>
+      </div>
+    </header>
   );
 }
 
